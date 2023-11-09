ad0316cfda7fd3055d44d445deeaeb598f902e41
dayeong <rhdek228@gmail.com>
2023-11-09 11:12:10 +0900

    연습문제 6-7 문제6-6의 메소드->인스턴스메서드로 변경

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,25 @@ import org.slf4j.LoggerFactory;
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-    static double getDistance(int x, int y, int x1, int y1) {
-        int a = x1-x; // 피타고라스 정리 선 a의 길이
-        int b = y1-y; // 피타고라스 정리 선 b의 길이
+    public static void main(String[] args) {
+        MyPoint m = new MyPoint(1,1);
 
-        return Math.sqrt(a*a+b*b); // 피타고라스 정리 c의 길이 (sprt:루트)
+        logger.debug(Double.toString(m.getDistance(2,2)));
+    }
+}
+
+class MyPoint {
+    int x;
+    int y;
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public static void main(String[] args) {
-            logger.debug(Double.toString(getDistance(1,1,2,2)));
+    double getDistance(int x1, int y1) {
+        int a = x1-this.x; // 피타고라스 정리 선 a의 길이
+        int b = y1-this.y; // 피타고라스 정리 선 b의 길이
+
+        return Math.sqrt(a*a+b*b); // 피타고라스 정리 c의 길이 (sprt:루트)
     }
 }
