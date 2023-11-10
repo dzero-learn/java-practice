5b9186ce2a8505abca2ce8266d3ab3f915c7909b
dayeong <rhdek228@gmail.com>
2023-11-10 17:47:03 +0900

    연습문제 6-23 배열 요소 최대값 구하기

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,50 @@ import org.slf4j.LoggerFactory;
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-    static boolean isNumber(String str) {
-        if(str == null || str.isEmpty()) {
-            return false;
+    static int max(int[] arr) {
+        if(arr == null || arr.length == 0) {
+            return -999999;
         }
 
-        return str.matches("[0-9]*");
+        int max = arr[0];
+
+        // 그냥 각 요소별로 비교하면서 큰 값 넣어두면 되는거였는데 굳이 정렬을 핸네,,ㅎ
+        for(int i=0; i<arr.length; i++) {
+            if(max < arr[i]) {
+                max = arr[i];
+            }
+        }
+
+        return max;
+
+        /* 내가 푼 거 :
+        // 원본값 유지하기 위해 배열 복사
+        int[] arrTmp = Arrays.copyOf(arr, arr.length);
+        arr = arrTmp;
+
+        // 오름차순 정렬
+        for(int i=0; i<arr.length-1; i++) {
+            boolean chk = false;
+            for (int j=0; j<arr.length-i-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                    chk = true;
+                }
+            }
+            if(!chk) break;
+        }
+
+        // 정렬 후 마지막 요소값이 최대값이므로 해당 값 리턴
+        return arr[arr.length-1];
+        */
     }
     public static void main(String[] args) {
-        String str = "123";
-        logger.debug(str + "는 숫자입니까? "+isNumber(str));
-
-        str = "1234ㅇ";
-        logger.debug(str+"는 숫자입니까? "+isNumber(str));
+        int[] data = {3,2,9,4,7};
+        logger.debug(java.util.Arrays.toString(data));
+        logger.debug("최대값:"+max(data));
+        logger.debug("최대값:"+max(null));
+        logger.debug("최대값:"+max(new int[]{})); // 크기가 0인 배열
     }
 }
