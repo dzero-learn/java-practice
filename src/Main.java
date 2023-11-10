104754d69ad0f92867d3249747b9bec5e9ba4b47
dayeong <rhdek228@gmail.com>
2023-11-10 16:22:14 +0900

    연습문제 6-20 배열 값 섞기(shuffle 클래스 작성)

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,29 @@ import org.slf4j.LoggerFactory;
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
+    static int[] shuffle(int[] arr) {
+        // 매개변수 유효성 검사
+        if(arr == null || arr.length == 0) {
+            return arr;
+        }
+
+        for(int i=0; i<arr.length; i++) {
+            int randomNum = (int)(Math.random()*arr.length); // 0~9
+
+            // 각 요소끼리 값 바꿔주기 = 섞기
+            int tmp = arr[i];
+            arr[i] = arr[randomNum];
+            arr[randomNum] = tmp;
+        }
+
+        return arr;
+    }
+
     public static void main(String[] args) {
-        MyPoint m = new MyPoint(1,1);
+        int[] original = {1,2,3,4,5,6,7,8,9};
+        logger.debug(java.util.Arrays.toString(original));
 
-        logger.debug(Double.toString(m.getDistance(2,2)));
-    }
-}
-
-class MyPoint {
-    int x;
-    int y;
-    public MyPoint(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    double getDistance(int x1, int y1) {
-        int a = x1-this.x; // 피타고라스 정리 선 a의 길이
-        int b = y1-this.y; // 피타고라스 정리 선 b의 길이
-
-        return Math.sqrt(a*a+b*b); // 피타고라스 정리 c의 길이 (sprt:루트)
+        int[] result = shuffle(original);
+        logger.debug(java.util.Arrays.toString(result));
     }
 }
