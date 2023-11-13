d5e7f230fc0af52d3ec3b8c652cebee5e69ca84e
dayeong <rhdek228@gmail.com>
2023-11-13 18:21:17 +0900

    예제 7-14 접근제어자 관련 Singleton 예제

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,33 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+import java.util.Objects;
 
-    static int abs(int n) {
-        return n < 0 ? -n : n; // n*(-1) 대신 부호만 바꿔줘도 됨
-    }
+public class Main {
+    public static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        int value = 5;
-        logger.debug(value+"의 절대값 :"+abs(value));
-        value = -10;
-        logger.debug(value+"의 절대값 :"+abs(value));
+        Singleton s = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        Singleton s3 = Singleton.getInstance();
+
+        logger.debug(Objects.toString(s));
+        logger.debug(Objects.toString(s2));
+        logger.debug(Objects.toString(s3));
+    }
+}
+
+class Singleton {
+    private static Singleton s = new Singleton();
+    
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        // 클래스 변수 s가 null인 경우만 인스턴스를 생성하므로 위에 생성한 인스턴스 하나만 사용하게 된다.
+        if(s == null)
+            s = new Singleton();
+
+        return s;
     }
 }
