4eca9c3e8367aee3712236adcc4350be4dc0c9a5
dayeoung <rhdek228@gmail.com>
2023-11-14 16:37:41 +0900

    연습문제 7-10,11 캡슐화,.this참조변수 사용

--- src/Main.java
+++ src/Main.java
@@ -1,81 +1,64 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-class SutdaDeck {
-    final int CARD_NUM = 20;
-    SutdaCard[] cards = new SutdaCard[CARD_NUM]; // 객체를 생성한 것 x
+class MyTv2 {
+    private boolean isPowerOn;
+    private int channel;
+    private int volume;
+    private int prvChannel;
 
-    SutdaDeck() {
-        for(int i=0; i<cards.length; i++) {
-            int num = i%10+1; // 카드 숫자
-            boolean isKwang = (i<10) && ( num == 1 || num == 3 || num == 8); // 이렇게 냅다 조건식을 적어도 되는거구나,,
+    final int MAX_VOLUME = 100;
+    final int MIN_VOLUME = 0;
+    final int MAX_CHANNEL = 100;
+    final int MIN_CHANNEL = 1;
 
-            cards[i] = new SutdaCard(num,isKwang);
-        }
+    public boolean isPowerOn() {
+        return isPowerOn;
     }
 
-    // 카드 섞기
-    void shuffle() {
-        for(int i=0; i<cards.length; i++) {
-            int tmp = (int)(Math.random()*cards.length);
-            SutdaCard sc;
-
-            sc = cards[i];
-            cards[i] = cards[tmp];
-            cards[tmp] = sc;
-        }
+    public void setPowerOn(boolean powerOn) {
+        this.isPowerOn = powerOn;
     }
 
-    // 지정된 위치의 카드 반환
-    SutdaCard pick(int index) {
-        if(index < 0 || index > CARD_NUM) {
-            return null;
-        }
-
-        return cards[index];
+    public int getChannel() {
+        return channel;
     }
 
-    // 임의의 위치 카드 반환
-    SutdaCard pick() {
-        return pick((int)(Math.random()*20));
-    }
-}
+    public void setChannel(int channel) {
+        this.prvChannel = this.channel;
 
-class SutdaCard {
-    int num;
-    boolean isKwang;
-
-    SutdaCard() {
-        this(1, true);
+        if(channel > MAX_CHANNEL) this.channel = MAX_CHANNEL;
+        else if(channel < MIN_CHANNEL) this.channel = MIN_CHANNEL;
+        else this.channel = channel;
     }
 
-    public SutdaCard(int num, boolean isKwang) {
-        this.num = num;
-        this.isKwang = isKwang;
+    public int getVolume() {
+        return volume;
     }
 
-    @Override
-    public String toString() {
-        return num + (isKwang ? "K":"");
+    public void setVolume(int volume) {
+        if(volume > MAX_VOLUME) this.volume = MAX_VOLUME;
+        else if(volume < MIN_VOLUME) this.volume = MIN_VOLUME;
+        this.volume = volume;
+    }
+
+    public void gotoPrevChannel() {
+        setChannel(this.prvChannel);
     }
 }
-
 public class Main {
     public static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        SutdaDeck deck = new SutdaDeck();
+        MyTv2 t = new MyTv2();
 
-        logger.debug(String.valueOf(deck.pick(0)));
-        logger.debug(String.valueOf(deck.pick()));
-        deck.shuffle();
-
-        String cardPrinter = "";
-        for(int i=0; i<deck.cards.length; i++) {
-            cardPrinter += deck.cards[i]+",";
-        }
-        logger.debug(cardPrinter);
-
-        logger.debug(String.valueOf(deck.pick(0)));
+        t.setChannel(10);
+        logger.debug("CH:"+t.getChannel());
+        t.setChannel(20);
+        logger.debug("CH:"+t.getChannel());
+        t.gotoPrevChannel();
+        logger.debug("CH:"+t.getChannel());
+        t.gotoPrevChannel();
+        logger.debug("CH:"+t.getChannel());
     }
 }
