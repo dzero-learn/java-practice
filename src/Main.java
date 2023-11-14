6ecbc11bb7d7aa3a00c4686f8a0bb470a7f93ea9
dayeoung <rhdek228@gmail.com>
2023-11-14 16:03:06 +0900

    연습문제 7-2 문제7-1에서 메소드 추가

--- src/Main.java
+++ src/Main.java
@@ -7,27 +7,37 @@ class SutdaDeck {
 
     SutdaDeck() {
         for(int i=0; i<cards.length; i++) {
-            // 내 문풀
-            if(i < 10) {
-                if(i==0) {
-                    cards[i] = new SutdaCard(); // 생성자 있는거 사용하려고 굳이 분기ㅎ
-                } else if(i == 2 || i == 7){ // 광 처리
-                    cards[i] = new SutdaCard(i+1,true);
-                } else {
-                    cards[i] = new SutdaCard(i+1,false);
-                }
-            } else { // 다시 숫자 1부터 넣어줌
-                cards[i] = new SutdaCard(i-9,false);
-            }
-
-            // 답지
-            /*
             int num = i%10+1; // 카드 숫자
             boolean isKwang = (i<10) && ( num == 1 || num == 3 || num == 8); // 이렇게 냅다 조건식을 적어도 되는거구나,,
 
             cards[i] = new SutdaCard(num,isKwang);
-             */
         }
+    }
+
+    // 카드 섞기
+    void shuffle() {
+        for(int i=0; i<cards.length; i++) {
+            int tmp = (int)(Math.random()*cards.length);
+            SutdaCard sc;
+
+            sc = cards[i];
+            cards[i] = cards[tmp];
+            cards[tmp] = sc;
+        }
+    }
+
+    // 지정된 위치의 카드 반환
+    SutdaCard pick(int index) {
+        if(index < 0 || index > CARD_NUM) {
+            return null;
+        }
+
+        return cards[index];
+    }
+
+    // 임의의 위치 카드 반환
+    SutdaCard pick() {
+        return pick((int)(Math.random()*20));
     }
 }
 
@@ -56,8 +66,16 @@ public class Main {
     public static void main(String[] args) {
         SutdaDeck deck = new SutdaDeck();
 
+        logger.debug(String.valueOf(deck.pick(0)));
+        logger.debug(String.valueOf(deck.pick()));
+        deck.shuffle();
+
+        String cardPrinter = "";
         for(int i=0; i<deck.cards.length; i++) {
-           logger.debug(deck.cards[i]+",");
+            cardPrinter += deck.cards[i]+",";
         }
+        logger.debug(cardPrinter);
+
+        logger.debug(String.valueOf(deck.pick(0)));
     }
 }
