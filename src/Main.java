7dd7e3f5defd80ca82801e318b81d466608b7aa5
dayeong <rhdek228@gmail.com>
2023-11-21 16:13:29 +0900

    연습문제 7-18 static 메소드, instanceof 사용

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,39 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-class MyTv2 {
-    private boolean isPowerOn;
-    private int channel;
-    private int volume;
-    private int prvChannel;
-
-    final int MAX_VOLUME = 100;
-    final int MIN_VOLUME = 0;
-    final int MAX_CHANNEL = 100;
-    final int MIN_CHANNEL = 1;
-
-    public boolean isPowerOn() {
-        return isPowerOn;
-    }
-
-    public void setPowerOn(boolean powerOn) {
-        this.isPowerOn = powerOn;
-    }
-
-    public int getChannel() {
-        return channel;
-    }
-
-    public void setChannel(int channel) {
-        this.prvChannel = this.channel;
-
-        if(channel > MAX_CHANNEL) this.channel = MAX_CHANNEL;
-        else if(channel < MIN_CHANNEL) this.channel = MIN_CHANNEL;
-        else this.channel = channel;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        if(volume > MAX_VOLUME) this.volume = MAX_VOLUME;
-        else if(volume < MIN_VOLUME) this.volume = MIN_VOLUME;
-        this.volume = volume;
-    }
-
-    public void gotoPrevChannel() {
-        setChannel(this.prvChannel);
-    }
-}
 public class Main {
     public static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
-        MyTv2 t = new MyTv2();
+    public static void action(Robot r) {
+        if(r instanceof DanceRobot) {
+            ((DanceRobot) r).dance();
+        } else if( r instanceof SingRobot) {
+            ((SingRobot) r).sing();
+        } else {
+            ((DrawRobot) r).draw();
+        }
+    }
 
-        t.setChannel(10);
-        logger.debug("CH:"+t.getChannel());
-        t.setChannel(20);
-        logger.debug("CH:"+t.getChannel());
-        t.gotoPrevChannel();
-        logger.debug("CH:"+t.getChannel());
-        t.gotoPrevChannel();
-        logger.debug("CH:"+t.getChannel());
+    public static void main(String[] args) {
+        Robot[] arr = { new DanceRobot(), new SingRobot(), new DrawRobot()};
+        for(int i=0; i< arr.length;i++)
+            action(arr[i]);
+    }
+}
+
+class Robot {}
+class DanceRobot extends Robot {
+    void dance() {
+        System.out.println("춤을 춥니다.");
+    }
+}
+class SingRobot extends Robot {
+    void sing() {
+        System.out.println("노래를 합니다.");
+    }
+}
+class DrawRobot extends Robot {
+    void draw() {
+        System.out.println("그림을 그립니다.");
     }
 }
